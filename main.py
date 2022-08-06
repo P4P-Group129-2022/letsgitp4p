6abfe5362b75749fd103a673795aeb5823fe57db
Admin <hpar461@aucklanduni.ac.nz>
2022-08-06 22:10:42 +1200

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def very_important_function():
+    print("This is a very important function")
+    return True
+
+def main():
+    very_important_function()
+
+if __name__ == "__main__":
+    main()
